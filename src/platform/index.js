0bfa5eec8b244ceb803aeb9daad1f59f588c93b3
ragingwind <ragingwind@gmail.com>
2015-11-04 13:28:36 +0900

    init

---
+++ src/platform/index.js
@@ -0,0 +1,49 @@
+'use strict';
+const app = require('app');
+const BrowserWindow = require('browser-window');
+const beacon = require('eddystone-beacon');
+
+// report crashes to the Electron project
+require('crash-reporter').start();
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+function createMainWindow() {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 400
+	});
+
+	win.loadUrl(`file://${__dirname}/../app/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+
+	beacon.advertiseUrl('http://naver.com');
+});
